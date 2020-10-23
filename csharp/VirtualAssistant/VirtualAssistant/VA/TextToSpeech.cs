4e6c2567c05c23d0fa79ad67487454f3144e95e1
yini yin <datacom@datacoms-MacBook-Pro.local>
2020-10-24 10:12:07 +1300

    add debug info for SpeechSynthesizer

--- csharp/VirtualAssistant/VirtualAssistant/VA/TextToSpeech.cs
+++ csharp/VirtualAssistant/VirtualAssistant/VA/TextToSpeech.cs
@@ -21,7 +21,24 @@ namespace VirtualAssistant.VA
         {
             using var synthesizer = new SpeechSynthesizer(config);
 
-            await synthesizer.SpeakTextAsync(txt);
+            var result = await synthesizer.SpeakTextAsync(txt);
+
+            if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine($"Speech synthesized to speaker for text [{txt}]");
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                    Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                }
+            }
         }
     }
 }
